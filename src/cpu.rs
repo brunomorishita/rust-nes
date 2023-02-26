47b0fd8a7360d3a9da30b44d0dc185fc81fb2066
Bruno Goncalves <bruno.slackware@gmail.com>
2023-02-25 21:37:31 -0300

    added BVC instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -77,6 +77,9 @@ lazy_static! {
         // If the negative flag is clear then add the relative displacement
         // to the program counter to cause a branch to a new location.
         OpCode::new(0x10, "BPL", 2, 2, AddressingMode::NoneAddressing),
+        // If the overflow flag is clear then add the relative displacement
+        // to the program counter to cause a branch to a new location.
+        OpCode::new(0x50, "BVC", 2, 2, AddressingMode::NoneAddressing),
         // Stores the contents of the accumulator into memory
         OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
         OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPage_X),
@@ -345,6 +348,11 @@ impl CPU {
                     "BPL" => {
                         self.program_counter += 1;
                         self.branch(false, 0b1000_0000);
+                        self.program_counter += (op.bytes - 1) as u16;
+                    }
+                    "BVC" => {
+                        self.program_counter += 1;
+                        self.branch(false, 0b0100_0000);
                         self.program_counter += (op.bytes - 1) as u16;
                     }
                     "STA" => {
@@ -624,6 +632,30 @@ mod test {
     fn test_bpl_backward() {
         let mut cpu = CPU::new();
         cpu.load(vec![0x10, 0x01, 0x00, 0xe8, 0xe8, 0x10, 0xfb, 0x00]);
+        cpu.reset();
+        cpu.status = 0;
+        cpu.register_x = 0x00;
+        cpu.run();
+
+        assert_eq!(cpu.register_x, 2);
+    }
+
+    #[test]
+    fn test_bvc_forward() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x50, 0x01, 0xe8, 0xe8, 0xe8, 0x00]);
+        cpu.reset();
+        cpu.status = 0;
+        cpu.register_x = 0x00;
+        cpu.run();
+
+        assert_eq!(cpu.register_x, 2);
+    }
+
+    #[test]
+    fn test_bvc_backward() {
+        let mut cpu = CPU::new();
+        cpu.load(vec![0x50, 0x01, 0x00, 0xe8, 0xe8, 0x50, 0xfb, 0x00]);
         cpu.reset();
         cpu.status = 0;
         cpu.register_x = 0x00;
